38e3019b5051c473957c757fc3960ddd220548f8
74maludin <74maludin@gmail.com>
2020-09-12 14:39:48 +0700

    rendering element: simple clock

--- src/index.js
+++ src/index.js
@@ -4,25 +4,28 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const formatName = (name) => {
-  return name.firstName + " " + name.lastname
+function tick() {
+  const name = {
+    firstName: "Muh",
+    lastname: "Jamaludin"
+  }
+
+  const element = (
+    <div>
+      <h1>Hello, {name.firstName + " " + name.lastname} !</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  )
+
+  ReactDOM.render(
+    element,
+    // <React.StrictMode>
+    //   <App />
+    // </React.StrictMode>,
+    document.getElementById('root')
+  );
 }
-
-const name={
-  firstName: "Muh",
-  lastname: "Jamaludin"
-}
-
-const element = <h1>Hello, {formatName(name)} !</h1>
-
-ReactDOM.render(
-  element,
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  document.getElementById('root')
-);
-
+setInterval(tick, 1000)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
