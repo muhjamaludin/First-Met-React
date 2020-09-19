3d4508a50ee5b72f5bffdbaeb32f803df5ecebab
74maludin <74maludin@gmail.com>
2020-09-20 05:45:28 +0700

    forms

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import * as serviceWorker from './serviceWorker';
 import Chapter06 from './Chapter06'
 import LoginControl from './Chapter07/LoginControl'
 import AttendanceBook from './Chapter08/AttendanceBook'
+import SignUp from './Chapter09/SignUp'
 
 // ReactDOM.render(<Chapter06/>, document.getElementById('root'))
 // ReactDOM.render(<LoginControl />, document.getElementById('root'))
-ReactDOM.render(<AttendanceBook />, document.getElementById('root'))
+// ReactDOM.render(<AttendanceBook />, document.getElementById('root'))
+ReactDOM.render(<SignUp />, document.getElementById('root'))
 
 // ReactDOM.render(
 //   <React.StrictMode>
